02ccba125f7a9750560b01676e79eacdc619748c
Jose <josejunior@id.uff.br>
2021-06-21 23:34:40 -0300

    adicionando testes da calculadora na pasta correta

--- test/calculadora/CalculadoraTest.java
+++ test/calculadora/CalculadoraTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import calculadora.Calculadora;
+
 @DisplayName("Classe para teste da calculadora")
 public class CalculadoraTest {
 	
@@ -50,10 +52,46 @@ public class CalculadoraTest {
 				() -> calc.divisao(8, 0));
 	}
 	
-	//** Meus Testes**
+	//**Meus Testes**
 	
 	@Test
-	public void testVerificaMultiplicacaoPorZero() {
-		assertEquals(0, calc.multiplicacao(9999,0))
+	public void testMultiplicacaoPorZero() {
+		Assertions.assertEquals(0, calc.multiplicacao(9999,0));
 	}
+	
+	@Test
+	public void testMultiplicacaoPorUm() {
+		assertEquals(9999, calc.multiplicacao(9999,1));
+	}
+	
+	@Test
+	public void testMultiplicacaoDeMesmoSinalSempreFicaPositivo() {
+		assertEquals(50, calc.multiplicacao(-2,-25));
+		assertEquals(50, calc.multiplicacao(2,25));
+	}
+	
+	@Test
+	public void testSomaZero() {
+		assertEquals(50, calc.soma(0,50));
+		assertEquals(50, calc.soma(50,0));
+	}
+	
+	@Test
+	public void testSomatoria() {
+		assertEquals(55, calc.somatoria(10));
+	}
+	
+	@DisplayName("Testa se o numero é positivo")
+	@Test
+	public void testVerificaNumeroPositivo() {
+		assertTrue(calc.ehPositivo(50));
+	}
+	
+	/*Precisa Falhar só esse*/
+	@DisplayName("Testa se o numero é positivo")
+	@Test
+	public void testVerificaNumeroNegativo() {
+		assertTrue(calc.ehPositivo(-50));
+	}
+	
 }
